0b34e6a4aeedde7c1996979fc39f5a43f100f049
mathewur <78866876+mathewur@users.noreply.github.com>
2021-03-01 20:47:30 +0000

    Java refresh exercises complete Urmila

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,14 +2,16 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
-    }
+	public boolean isFromManchester(Person person) {
+		// check if the person's city is Manchester
+		return person.getCity().equalsIgnoreCase(Person.getManchesterCity());
+	}
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
-    }
-    
+	public boolean canWatchFilm(Person person, int ageLimit) {
+		// check if the person's age is over limit
+		//System.out.println("person.getAge() : compare with limit "+person.getAge()+(person.getAge() >= ageLimit));
+
+		return (person.getAge() >= ageLimit);
+	}
+
 }
